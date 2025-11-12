63393b0e537114199495cc01415bade6a6bc1465
SpacePheonix <felixcha.2005@gmail.com>
2025-11-12 17:02:55 -0500

    Avancée finale pour la journée

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,12 +1,27 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
     this.init();
+
+    this.initSwiperPagination();
   }
 
   init() {
     Icons.load();
+  }
+
+  initSwiperPagination() {
+    const target = document.querySelector('.js-swiper-page');
+
+    if (target) {
+      const swiper = new Swiper(target, {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    }
   }
 }
 new Main();
